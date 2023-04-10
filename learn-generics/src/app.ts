a3bed9bbba63716ebedb0b89b37a627f8c26fea4
jhin3283 <hj3283@gmail.com>
2023-04-10 16:18:44 +0900

    Add: Generics

--- learn-generics/src/app.ts
+++ learn-generics/src/app.ts
@@ -10,3 +10,9 @@
 //     data.split(' ')
 // })
 
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+// console.log(merge({name: 'Max'},{age: 30}))
+const megedObj = merge({name: 'Max'},{age: 30})
